ba44e5e3dbae8ac845b04d49667875bc00519ac6
GeovaniPassos <geovanipassos95@outlook.com>
2025-05-02 08:37:54 -0300

    Ajustando a forma de mostrar a lista de alimentos

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,12 @@
 import { MatTableModule, MatTableDataSource} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListItem, MatListModule} from '@angular/material/list';
 
 export const MATERIAL_MODULES = [
   MatTableModule,
   MatInputModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  MatListModule,
+  MatListItem
 ]
